46884b343ecd03ef43a134a2c8166dd8351da39d
danielronalds <nzdanron@gmail.com>
2022-08-12 23:12:10 +1200

    Added basic functionality of reading tasks from a csv file and returning a Vec<Task> to the main method. It's rough code, that I'll probably refactor later, however it gets the job done for now. Now I can move onto writing tasks into a file

--- src/actions/file_management.rs
+++ src/actions/file_management.rs
@@ -1,9 +1,13 @@
-use std::{path::Path, fs::File};
+use std::{path::Path, fs::File, io::BufReader, io::BufRead};
+
+const FILENAME: &str = ".tasks";
+
+use crate::task::{Task, TaskStatus};
 
 // Function to create a .tasks file to store tasks in csv format
 pub fn init_list() {
     // Checks to see if a .tasks file exists already
-    if !Path::new("./.tasks").exists() {
+    if !Path::new(FILENAME).exists() {
         // If it doesn't create a .tasks file
         match File::create("./.tasks") {
            Ok(fc) => {
@@ -17,4 +21,57 @@ pub fn init_list() {
         // If the file already exists, inform the user
         println!("A tasks list already exists!");   
     }
+}
+
+// Function to read the task file into a Vec of tasks
+pub fn read_task_list() -> Vec<Task> {
+    // Open the file where tasks are stored
+    let file = File::open(FILENAME).expect("Could not open file!");
+
+    // Declare a reader for the file
+    let buf_reader = BufReader::new(file);
+    
+    // Don't quite understand this line fully
+    let lines: Vec<String> = buf_reader.lines() // This makes sense, 
+        .map(|l| l.expect("Could not parse line")) // ...now this is a bit of a mystery
+        .collect(); // This also makes sense, its collecting everything into a vec
+
+    // Go through every line and add the task to a tasklist that the method return
+    let mut task_list: Vec<Task> = Vec::new();
+
+    for line in lines {
+        let task_vec = read_csv_line(line);
+        
+        // Figuring out the task's status
+        if task_vec.len() == 2 {
+            let task_status = match task_vec[1].as_str() {
+                "Completed"  => TaskStatus::Completed,
+                "InProgress" => TaskStatus::InProgress,
+                "NotStarted" => TaskStatus::NotStarted,
+                &_ => TaskStatus::NotStarted,
+            };
+
+            let task_description = &task_vec[0];
+
+            task_list.push(
+                Task { desc: task_description.to_string(), status: task_status }
+            );
+        } else {
+            println!("ERROR WITH LINE");
+        }
+    }
+
+    task_list
+}
+// Function to read a string in csv formating
+fn read_csv_line(line: String) -> Vec<String>{
+    let data_points: Vec<&str> = line.split(",").collect();
+
+    let mut data:Vec<String> = Vec::new();
+
+    for data_point in data_points {
+        data.push(data_point.to_string());
+    }
+
+    data
 }
--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,7 @@ mod actions;
 mod task;
 
 fn main() {
-    let mut tasks: Vec<Task> = Vec::new();
-
-    actions::task_management::add_task(&mut tasks, String::from("Just a basic task"));
-    actions::task_management::add_task(&mut tasks, String::from("Another basic task"));
-    actions::task_management::add_task(&mut tasks, String::from("Yet another basic task"));
+    let tasks = actions::file_management::read_task_list();
 
     actions::task_management::list_tasks(&tasks);
-
-    actions::task_management::start_task(&mut tasks[0]);
-
-    actions::task_management::finish_task(&mut tasks[1]);
-
-    actions::task_management::remove_task(&mut tasks, 2);
-    
-    actions::task_management::remove_task(&mut tasks, 3);
-
-    actions::file_management::init_list();
 }
