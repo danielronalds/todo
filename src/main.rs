f66ba244b8ac70d5b0e4add6de79cf400b02e08d
danielronalds <nzdanron@gmail.com>
2022-08-17 10:07:13 +1200

    Done some code refactoring, no features added just attempting to start cleaning up the code

--- src/main.rs
+++ src/main.rs
@@ -7,50 +7,75 @@ mod task;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let command;
-
-    // Checks to see if an argument has been provided or not
-    if args.len() > 1 {
-        command = args[1].as_str();
-    } else {
-        eprint!("No command!");
+    let config = Config::build(args).unwrap_or_else(|err| {
+        println!("{}", err);
         process::exit(1);
-    };
+    });
+    
+    run(config);
+}
 
+
+struct Config {
+    command: String,
+    command_arg: String,
+}
+
+impl Config {
+    fn build(args: Vec<String>) -> Result<Config, &'static str> {
+        // Checks to see if any command was supplied
+        if args.len() < 2  {
+            return Err("No command supplied!");
+        }
+
+        // Setting the arguments
+        let cmd = args[1].clone();
+        let mut cmd_arg = String::new();
+
+        // Checks to see if a command argument was supplied, setting it if it was 
+        if args.len() > 2 {
+            cmd_arg = args[2].clone();
+        } 
+
+        Ok(Config {
+            command: cmd,
+            command_arg: cmd_arg,
+        })
+    }
+}
+
+
+fn run(config: Config) {
+    // To prevent accessing the task list if it doesn't exit
+    if config.command.as_str() == "init" {
+        actions::file_management::init_list();
+        process::exit(1);
+    }
+    
+    let mut tasks = actions::file_management::read_task_list();
 
     // This is shit, hobbled together code. However for now it works, which means
     // this program by bare definition, does something!
-    match command {
-        // Init a task list
-        "init" => actions::file_management::init_list(),
+    match config.command.as_str() {
         // List the current tasks
-        "list" => {
-            let tasks = actions::file_management::read_task_list();
-            actions::task_management::list_tasks(&tasks);
-            actions::file_management::save_task_list(tasks);
-        },
+        "list" => actions::task_management::list_tasks(&tasks),
         // Add a task
         "add" => {
-            let mut tasks = actions::file_management::read_task_list();
-            let sub_command = args[2].as_str();
-            actions::task_management::add_task(&mut tasks, sub_command.to_string());
-            actions::file_management::save_task_list(tasks);
+            let task_desc = config.command_arg;
+            actions::task_management::add_task(&mut tasks, task_desc);
         },
         // Delete a task
         "delete" => {
-            let mut tasks = actions::file_management::read_task_list();
-            let task_index = args[2].parse().unwrap_or_else(|_| {
+            let task_index = config.command_arg.parse().unwrap_or_else(|_| {
                 eprintln!("That is not a valid Task ID!");
                 process::exit(1);
             });
             actions::task_management::remove_task(&mut tasks, task_index);
-            actions::file_management::save_task_list(tasks);
         },
         // Start a task
         "start" => {
-            let mut tasks = actions::file_management::read_task_list();
             // Parsing second argument into an index for accessing the task vec
-            let task_index: usize = args[2].parse().unwrap_or_else(|_| {
+            let task_index: usize = config.command_arg.parse().unwrap_or_else(|_| {
                 eprintln!("That is not a valid Task ID!");
                 process::exit(1);
             });
@@ -60,13 +85,11 @@ fn main() {
                 process::exit(1);
             }
             actions::task_management::start_task(&mut tasks[task_index]);
-            actions::file_management::save_task_list(tasks);
         },
         // Finish a task
         "finish" => {
-            let mut tasks = actions::file_management::read_task_list();
             // Parsing second argument into an index for accessing the task vec
-            let task_index: usize = args[2].parse().unwrap_or_else(|_| {
+            let task_index: usize = config.command_arg.parse().unwrap_or_else(|_| {
                 eprintln!("That is not a valid Task ID!");
                 process::exit(1);
             });
@@ -76,10 +99,10 @@ fn main() {
                 process::exit(1);
             }
             actions::task_management::finish_task(&mut tasks[task_index]);
-            actions::file_management::save_task_list(tasks);
         },
         // If the user has not typed a valid command, inform them
         _ => eprintln!("Unrecognised command!"),
     }
 
+    actions::file_management::save_task_list(tasks);
 }
