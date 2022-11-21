3fea815322878b7e5657c77f6d98215d86814656
Daniel R <nzdanron@gmail.com>
2022-11-21 18:35:16 +1300

    Added methods for updating the description and status of a task, but havent written the functionality yet. Written failing tests for the update_desciption() method

--- src/task.rs
+++ src/task.rs
@@ -52,6 +52,16 @@ impl Task {
     pub fn status(&self) -> TaskStatus {
         self.status.clone()
     }
+
+    /// Updates the description of the task
+    pub fn update_description(&self, new_description: String) -> Result<(), Errors> {
+        Ok(())
+    }
+
+    /// Updates the status of the task
+    pub fn update_status(&self, new_status: TaskStatus) -> Result<(), Errors> {
+        Ok(())
+    }
 }
 
 /// Unit tests
@@ -96,5 +106,59 @@ mod tests {
         let task_error = Task::new(description, TaskStatus::InProgress).unwrap_err();
 
         assert_eq!(task_error, Errors::InvalidCharInDescription)
+    }
+
+    #[test]
+    /// Checks if the update_status method works
+    fn update_status_works() {
+        let description = String::from("This is a basic task!");
+
+        let task = Task::new(description, TaskStatus::NotStarted).unwrap();
+
+        task.update_status(TaskStatus::InProgress).unwrap();
+
+        assert_eq!(task.status(), TaskStatus::InProgress)
+    }
+
+    #[test]
+    /// Checks if the update_description method works
+    fn update_description_works() {
+        let description = String::from("This is the first description");
+
+        let task = Task::new(description, TaskStatus::InProgress).unwrap();
+
+        let new_description = String::from("The is the new description");
+
+        task.update_description(new_description.clone()).unwrap();
+        
+        assert_eq!(task.description(), new_description)
+    }
+
+    #[test]
+    /// Checks if the update_description fails when passed an empty description
+    fn update_description_fails_on_empty_description() {
+        let description = String::from("This is the first description");
+
+        let task = Task::new(description, TaskStatus::InProgress).unwrap();
+
+        let new_description = String::new();
+
+        let err = task.update_description(new_description.clone()).unwrap_err();
+        
+        assert_eq!(err, Errors::EmptyDescription)
+    }
+
+    #[test]
+    /// Checks if the update_description fails when passed a description with an invalid char
+    fn update_description_fails_on_invalid_char() {
+        let description = String::from("This is the first description");
+
+        let task = Task::new(description, TaskStatus::InProgress).unwrap();
+
+        let new_description = String::from("This invalid char | cannot be in the description");
+
+        let err = task.update_description(new_description.clone()).unwrap_err();
+        
+        assert_eq!(err, Errors::InvalidCharInDescription)
     }
 }
