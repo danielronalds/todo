0509b032e6f1f23ef7ddfb6e86df676cafe7294a
Daniel R <nzdanron@gmail.com>
2022-11-21 17:04:52 +1300

    Starting fresh! deleted everything and have got a basic hello world program. Even deleted the task list

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use std::{env, process::exit};
-
-use todo::{self, Config};
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(args).unwrap_or_else(|err| {
-        todo::print_error(format!("{}", err).as_str());
-        exit(1);
-    });
-    
-    todo::run(config);
+    println!("Hello, world!");
 }
