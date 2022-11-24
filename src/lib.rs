e6e06cf08d13b260cda25c49863979168122f6c6
Daniel R <nzdanron@gmail.com>
2022-11-24 23:13:27 +1300

    So, ive thought about it for all of three minutes, lib.rs is going to have public facing functions for each command, that handle the heavy lifting. main.rs will pass the arguments and have the command tree structure... in theory this makes sense and should be organised and easy to work with

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,30 @@
-// Modules
-mod args;
-mod task;
-mod commands;
+// Public facing modules
+pub mod args;
+pub mod task;
+
+// Private modules
 mod task_management;
 
-use clap::Parser;
+use crate::task::{Task, TaskStatus, TaskErrors};
+use crate::args::AddCommand;
 
-use args::TodoArgs;
-
-use task::Task;
-
-/// The main run function of the library
+/// Creates a new task. This handles any errors and returns an appropriate error message
+/// This approach will most likely change however, or this function moved
 ///
-/// This code handles parsing of the arguments and the main code calling
-pub fn run() {
-    let args = TodoArgs::parse();
+/// Parameters
+/// arguments:   The arguments for the command from the cli
+pub fn new_task(arguments: AddCommand) -> Result<Task, &'static str> {
+    let task = match Task::new(arguments.description, TaskStatus::NotStarted) {
+        Ok(task) => task,
+        Err(err) => match err {
+            TaskErrors::EmptyDescription => {
+                return Err("Tasks cannot have empty descriptions!");
+            }
+            TaskErrors::InvalidCharInDescription => {
+                return Err("Tasks cannot have the | char!");
+            }
+        },
+    };
 
-    let mut tasks_vec: Vec<Task> = Vec::new();
-
-    match args.command {
-        args::Commands::Add(arguments) => {
-            match commands::new_task(arguments) {
-                Ok(task) => tasks_vec.push(task),
-                Err(err) => println!("{}", err),
-            };
-        }
-    }
-
-    println!("{:?}", tasks_vec);
+    Ok(task)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-use todo;
+use clap::Parser;
+
+use todo::args;
+use todo::args::TodoArgs;
+
+use todo::task::Task;
 
 fn main() {
-    todo::run();
+    let args = TodoArgs::parse();
+
+    let mut tasks_vec: Vec<Task> = Vec::new();
+
+    match args.command {
+        args::Commands::Add(arguments) => {
+            match todo::new_task(arguments) {
+                Ok(task) => tasks_vec.push(task),
+                Err(err) => println!("{}", err),
+            };
+        }
+    }
+
+    println!("{:?}", tasks_vec);
 }
